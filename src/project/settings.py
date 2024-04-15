4d63d7699ebe4fc7ce653d03575bffdc78f6a53e
prathameshkhade <pkhade2865@gmail.com>
2024-04-15 04:21:39 -0400

    add: ahana template
    added the whole template. not loading css and js

--- src/project/settings.py
+++ src/project/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+TEMPLATES_DIR = os.path.join(BASE_DIR, "src/templates")
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-2ew(ylg$s=v#ti4f-w1l@_7kb6=+xjvwiz^$kj=m3mj99w^h&_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] 
 
 
 # Application definition
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'src.project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "src/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# my own settings
+
--- src/project/urls.py
+++ src/project/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Custom imports
+from .views import homepage_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage_view)
 ]
