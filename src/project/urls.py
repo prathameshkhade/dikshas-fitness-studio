d30bbf6dd1f1d1df84a8673ffc0c574aa9cf2e56
prathameshkhade <pkhade2865@gmail.com>
2024-04-16 04:32:46 -0400

    add: contact page with contact.html
    not loading css and js, need to updated href

--- src/project/urls.py
+++ src/project/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 
 # Custom imports
-from .views import homepage_view
+from .views import homepage_view, contact_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homepage_view)
+    path('', homepage_view),
+    path('contact/', contact_view),
+
 ]
--- src/project/views.py
+++ src/project/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here
 def homepage_view(request):  
     return render(request, "index.html")
+
+def contact_view(request):  
+    return render(request, "contact.html")
