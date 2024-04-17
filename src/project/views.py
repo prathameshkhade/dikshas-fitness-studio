feee1a5a2d7141b1ae5c95aa3ca76cf2865d15b2
prathameshkhade <pkhade2865@gmail.com>
2024-04-17 01:28:25 -0400

    fix: not loading static files in contact
    changed everywhere: load static

--- src/project/views.py
+++ src/project/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here
 def homepage_view(request):  
-    return render(request, "index.html")
+    return render(request, "home.html")
 
 def contact_view(request):  
     return render(request, "contact.html")
