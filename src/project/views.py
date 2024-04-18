bc33e8d294bb2970ded1fd8d606a540265c43713
prathameshkhade <pkhade2865@gmail.com>
2024-04-18 01:10:56 -0400

    add: data comming from table to page

--- src/project/views.py
+++ src/project/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from src.dynamic.models import AboutPage
+
 # Create your views here
 def homepage_view(request):  
     return render(request, "home.html")
@@ -8,4 +10,20 @@ def contact_view(request):
     return render(request, "contact.html")
 
 def about_view(request):  
-    return render(request, "about.html")
+    table_data = AboutPage.objects.get(id=1)
+
+    data = {
+        'name': table_data.name,
+        'title': table_data.title,
+        'experience': table_data.experience,
+        'email': table_data.email,
+        'bio': table_data.bio,
+        'paragraph_title': table_data.paragraph_title,
+        'paragraph_description': table_data.paragraph_description
+    }
+
+    print("-"*30)
+    print(data)
+    print("-"*30)
+
+    return render(request, "about.html", data)
