35ea3f42716da987f33de4adc974fbc40a9117d6
prathameshkhade <pkhade2865@gmail.com>
2024-04-18 04:00:28 -0400

    add: Hyperlinks option for admin page

--- src/dynamic/admin.py
+++ src/dynamic/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AboutPage
+from .models import AboutPage, Hyperlinks
 
 class Service(admin.ModelAdmin):
     list_display= (
@@ -15,3 +15,4 @@ class Service(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(AboutPage, Service)
+admin.site.register(Hyperlinks)
--- src/dynamic/models.py
+++ src/dynamic/models.py
@@ -13,3 +13,8 @@ class AboutPage(models.Model):
 
     class Archivements:
         archivements = models.CharField(max_length=120)
+
+class Hyperlinks(models.Model):
+    facebook = models.CharField(("facebook link"), max_length=200)
+    instagram = models.CharField(("instagram link"), max_length=200)
+    youtube = models.CharField(("youtube link"), max_length=1024)
