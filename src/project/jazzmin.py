96e030cbdaebe89dfe2e9dc56b95a45401b92e63
prathameshkhade <pkhade2865@gmail.com>
2024-04-23 00:39:31 -0400

    change: admin panel a little

---
+++ src/project/jazzmin.py
@@ -0,0 +1,56 @@
+import os.path
+from pathlib import Path
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Fitness Studio Admin",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Fitness Studio",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "Fitness Studio",
+
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    # "site_logo": "../static/img/logo.png",
+
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    # "site_icon": None,
+
+    # Welcome text on the login screen
+    "welcome_sign": "Welcome to the Fitness Studio Admin",
+
+    # Copyright on the footer
+    "copyright": "Fitness Studio Ltd",
+
+    # List of model admins to search from the search bar, search bar omitted if excluded
+    # If you want to use a single search field you dont need to use a list, you can use a simple string 
+    "search_model": ["auth.User", "auth.Group"],
+
+    #############
+    # Side Menu #
+    #############
+
+    # Whether to display the side menu
+    "show_sidebar": True,
+
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
+
+    #################
+    # Related Modal #
+    #################
+    # Use modals instead of popups
+    # "related_modal_active": False,
+
+    #############
+    # UI Tweaks #
+    #############
+    # Relative paths to custom CSS/JS scripts (must be present in static files)
+    # "custom_css": None,
+    # "custom_js": None,
+    # Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
+    "use_google_fonts_cdn": True,
+    # Whether to show the UI customizer on the sidebar
+    # "show_ui_builder": False,
+}
--- src/project/settings.py
+++ src/project/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os.path
 from pathlib import Path
 
+# Settings for the Jazzmin admin panel
+from .jazzmin import JAZZMIN_SETTINGS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
