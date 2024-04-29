285bd417478a9ab530c5429802de8def1adadb57
prathameshkhade <pkhade2865@gmail.com>
2024-04-29 06:04:42 -0400

    fix: changed path to src.manage

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 src.manage collectstatic --noinput --clear
 echo "BUILD END"
