ca1cd2709669a73cb08d0cb8adeb981ac2a9c23d
Prathamesh Khade <pkhade2865@gmail.com>
2024-06-30 19:58:15 +0530

    add: animation top section of navbar

--- src/static/js/navbarAnimation.js
+++ src/static/js/navbarAnimation.js
@@ -2,7 +2,14 @@
 
 const tl = gsap.timeline();
 
-
+tl.from('.header-info', {
+    duration: 1,
+    delay: 0.9,
+    stagger: 0.2,
+    x: 500,
+    opacity: 0,
+    ease: 'power3.inOut'
+}, 'test')
 tl.from('.header-bottom a', {
     duration: 1,
     delay: 0.9,
@@ -10,7 +17,7 @@ tl.from('.header-bottom a', {
     stagger: 0.2,
     opacity: 0,
     ease: 'power3.inOut'
-})
+}, 'test')
 tl.from('#infor-switch', {
     x: 100,
     opacity: 0,
