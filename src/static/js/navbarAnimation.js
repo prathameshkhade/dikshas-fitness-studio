00bffd298d2fdb36fc568356abc07ba086c17275
Prathamesh Khade <pkhade2865@gmail.com>
2024-06-30 19:44:07 +0530

    add: navbar animation

---
+++ src/static/js/navbarAnimation.js
@@ -0,0 +1,18 @@
+// var elements = document.querySelectorAll('.header-section .header-bottom .container .main-menu li a');
+
+const tl = gsap.timeline();
+
+
+tl.from('.header-bottom a', {
+    duration: 1,
+    delay: 0.9,
+    x: 500,
+    stagger: 0.2,
+    opacity: 0,
+    ease: 'power3.inOut'
+})
+tl.from('#infor-switch', {
+    x: 100,
+    opacity: 0,
+    ease: 'power3.inOut'
+})
